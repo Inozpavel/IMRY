af6a4d440a4fa173efe3a74617ba17ba28e64baf
Inozpavel <Inozpavel@mail.ru>
2020-10-15 20:38:42 +0300

    Исправлена проверка корректности ввода

--- Views/InputWorkNumberBox.xaml.cs
+++ Views/InputWorkNumberBox.xaml.cs
@@ -27,14 +27,19 @@ namespace WorkReportCreator.Views
         {
             string text = textBox.Text;
 
-            if (text.Trim().All(x => char.IsDigit(x) == false))
+            if (text.Trim().All(x => char.IsDigit(x)) == false)
             {
-                MessageBox.Show("Введенный текст не является числом!\nПроверьте данные.", "Недопустимое значение!", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Для ввода разрешены только цифры!\nПроверьте данные.", "Недопустимое значение!", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
-            else if (_existingWorks.Contains(text.Trim()))
+            else if (_existingWorks.Contains(int.Parse(text.Trim()).ToString()))
             {
                 MessageBox.Show("Введенное значение уже есть в списке!\nОдинаковых значений быть не должно!", "Недопустимое значение!", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            else if (text.Trim().Length > 3)
+            {
+                MessageBox.Show("Введенное значение слишком большое!", "Недопустимое значение!", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
             ResultNumber = int.Parse(text.Trim());
