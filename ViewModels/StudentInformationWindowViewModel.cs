3e6e18c5f6bc8b98d6f1c5655c89b28834372d4c
Inozpavel <Inozpavel@mail.ru>
2020-10-18 13:32:29 +0300

    Изменена ViewModel

--- ViewModels/StudentInformationWindowViewModel.cs
+++ ViewModels/StudentInformationWindowViewModel.cs
@@ -1,24 +1,105 @@
 ﻿using Microsoft.Win32;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 using WorkReportCreator.Models;
 
 namespace WorkReportCreator.ViewModels.Commands
 {
     internal class StudentInformationWindowViewModel : INotifyPropertyChanged
     {
+        private readonly WorksAndStudentInfoWindow _worksAndStudentInfoWindow;
+
+        private Visibility _labaratoriesVisibility;
+
+        private Visibility _practicesVisibility;
+
+        private Visibility _worksSelectVisibility;
+
+        /// <summary>
+        /// От значения зависит, все кнопки с практическими работами будут отмечены / не отмечены
+        /// </summary>
+        private bool _shouldCheckAllPracticalWorks = false;
+
+        /// <summary>
+        /// От значения зависит, все кнопки с лабораторными работами будут отмечены / не отмечены
+        /// </summary>
+        private bool _shouldCheckAllLaboratoryWork = false;
+
         public Command SaveStudentInfo { get; private set; }
+
         public Command LoadStudentInfo { get; private set; }
+
+        public Command CheckAllPracticalButtons { get; private set; }
+
+        public Command CheckAllLaboratoryButtons { get; private set; }
+
+        public Command ShowReportsPage { get; private set; }
+
+        /// <summary>
+        /// Видимость блока с выбором практических работ
+        /// </summary>
+        public Visibility PracticesVisibility
+        {
+            get => _practicesVisibility;
+            set
+            {
+                _practicesVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Видимость блока с выбором лабораторных работ
+        /// </summary>
+        public Visibility LaboratoriesVisibility
+        {
+            get => _labaratoriesVisibility;
+            set
+            {
+                _labaratoriesVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Видимость всего блока с выбором работ
+        /// </summary>
+        public Visibility WorksSelectVisibility
+        {
+            get => _worksSelectVisibility;
+            set
+            {
+                _worksSelectVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Список всех кнопок для выбора номеров практических работ
+        /// </summary>
+        public List<ToggleButton> PracticalWorksButtons { get; set; } = new List<ToggleButton>();
+
+        /// <summary>
+        /// Список всех кнопок для выбора номеров лабораторных работ
+        /// </summary>
+        public List<ToggleButton> LaboratoryWorksButtons { get; set; } = new List<ToggleButton>();
+
 
         /// <summary>
         /// Вся информация о студенте
         /// </summary>
         private StudentInformation _student = new StudentInformation();
-
+        
+        /// <summary>
+        /// Текущая информация о студенте
+        /// </summary>
         public StudentInformation Student
         {
             get => _student;
@@ -31,13 +112,82 @@ namespace WorkReportCreator.ViewModels.Commands
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public StudentInformationWindowViewModel()
+        public StudentInformationWindowViewModel(WorksAndStudentInfoWindow window)
         {
+            _worksAndStudentInfoWindow = window;
             SaveStudentInfo = new Command(SaveStudent, null);
             LoadStudentInfo = new Command(LoadStudent, null);
+            CheckAllLaboratoryButtons = new Command((sender) => CheckAllButtons(LaboratoryWorksButtons, ref _shouldCheckAllLaboratoryWork), null);
+            CheckAllPracticalButtons = new Command((sender) => CheckAllButtons(PracticalWorksButtons, ref _shouldCheckAllPracticalWorks), null);
+            ShowReportsPage = new Command(LoadReportsPage, null);
 
             MainParams mainParams = new MainParams();
             LoadStudent(mainParams.UserDataFileName);
+
+            ResourceDictionary dictionaryWithTiltedButton = new ResourceDictionary() { Source = new Uri("/Views/Styles/TiltedButtonStyle.xaml", UriKind.Relative) };
+            ResourceDictionary dictionaryWithNumberToggleButton = new ResourceDictionary() { Source = new Uri("/Views/Styles/NumberToggleButtonStyle.xaml", UriKind.Relative) };
+
+            ToggleButton checkAllPracticalButton = new ToggleButton() { Content = "Все работы", Style = dictionaryWithTiltedButton["TiltedButton"] as Style, Command = CheckAllPracticalButtons, Margin = new Thickness(10) };
+            ToggleButton checkAllLaboratoriesButton = new ToggleButton() { Content = "Все работы", Style = dictionaryWithTiltedButton["TiltedButton"] as Style, Command = CheckAllLaboratoryButtons, Margin = new Thickness(10) };
+
+            PracticalWorksButtons.Add(checkAllPracticalButton);
+            LaboratoryWorksButtons.Add(checkAllLaboratoriesButton);
+
+            try
+            {
+                var template = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, ReportInformation>>>(File.ReadAllText(mainParams.CurrentTemplateFilePath));
+                foreach (string type in template.Keys.Distinct())
+                {
+                    foreach (string workNumber in template[type].Keys.Distinct())
+                    {
+                        ToggleButton button = new ToggleButton() { Content = workNumber, Style = dictionaryWithNumberToggleButton["NumberToggleButton"] as Style };
+                        if (type == "Practices")
+                        {
+                            PracticalWorksButtons.Add(button);
+                        }
+                        else if (type == "Laboratories")
+                        {
+                            LaboratoryWorksButtons.Add(button);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось загрузить номера работ из шаблона!\nПроверьте коррекность пути!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw new Exception();
+            }
+            PracticesVisibility = PracticalWorksButtons.Count > 1 ? Visibility.Visible : Visibility.Collapsed;
+            LaboratoriesVisibility = LaboratoryWorksButtons.Count > 1 ? Visibility.Visible : Visibility.Collapsed;
+            WorksSelectVisibility = PracticalWorksButtons.Count > 1 || LaboratoryWorksButtons.Count > 1 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Создает окно с работами и показывает его
+        /// </summary>
+        private void LoadReportsPage(object sender)
+        {
+            List<string> selectedLaboratoryWorks = LaboratoryWorksButtons.Where(x => x.IsChecked ?? false).Select(x => x.Content as string).ToList();
+            List<string> selectedPracticalWorks = PracticalWorksButtons.Where(x => x.IsChecked ?? false).Select(x => x.Content as string).ToList();
+            try
+            {
+                ReportsWindow reportsPage = new ReportsWindow(_worksAndStudentInfoWindow, selectedLaboratoryWorks, selectedPracticalWorks, Student);
+                _worksAndStudentInfoWindow.Hide();
+                reportsPage.Show();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Отмечает / снимает отметки со всех кнопок с практическими / лабораторными работами
+        /// </summary>
+        private void CheckAllButtons(List<ToggleButton> buttons, ref bool oldValue)
+        {
+            bool newValue = !oldValue;
+            buttons.ForEach(x => x.IsChecked = newValue);
+            oldValue = newValue;
         }
 
         /// <summary>
--- Views/WorskAndStudentInfoWindow.xaml.cs
+++ Views/WorskAndStudentInfoWindow.xaml.cs
@@ -1,12 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using WorkReportCreator.Models;
+﻿using System.Windows;
 using WorkReportCreator.ViewModels.Commands;
 
 namespace WorkReportCreator
@@ -19,61 +11,13 @@ namespace WorkReportCreator
         /// <summary>
         /// Модель данных этого элемента
         /// </summary>
-        private readonly StudentInformationWindowViewModel _model = new StudentInformationWindowViewModel();
-
-        /// <summary>
-        /// От значения зависит, все кнопки с практическими работами будут отмечены / не отмечены
-        /// </summary>
-        private bool _shouldCheckAllPracticalWorks = true;
-
-        /// <summary>
-        /// От значения зависит, все кнопки с лабораторными работами будут отмечены / не отмечены
-        /// </summary>
-        private bool _shouldCheckAllLaboratoryWork = true;
-
-        /// <summary>
-        /// Список всех кнопок для выбора номеров практических работ
-        /// </summary>
-        private readonly List<ToggleButton> _practicalWorksButtons = new List<ToggleButton>();
-
-        /// <summary>
-        /// Список всех кнопок для выбора номеров лабораторных работ
-        /// </summary>
-        private readonly List<ToggleButton> _laboratoryWorksButtons = new List<ToggleButton>();
+        private readonly StudentInformationWindowViewModel _model;
 
         public WorksAndStudentInfoWindow()
         {
             InitializeComponent();
+            _model = new StudentInformationWindowViewModel(this);
             DataContext = _model;
-
-            MainParams mainParams = new MainParams();
-
-            try
-            {
-                var template = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, ReportInformation>>>(File.ReadAllText(mainParams.CurrentTemplateFilePath));
-                foreach (string type in template.Keys.Distinct())
-                {
-                    foreach (string workNumber in template[type].Keys.Distinct())
-                    {
-                        ToggleButton button = new ToggleButton() { Content = workNumber, Style = stackPanelWithWorks.Resources["NumberButton"] as Style };
-                        if (type == "Practices")
-                        {
-                            _practicalWorksButtons.Add(button);
-                            stackPanelPracticalWorks.Children.Add(button);
-                        }
-                        else if (type == "Laboratories")
-                        {
-                            _laboratoryWorksButtons.Add(button);
-                            stackPanelLaboratoryWorks.Children.Add(button);
-                        }
-                    }
-                }
-            }
-            catch(Exception)
-            {
-                MessageBox.Show("Не удалось загрузить номера работ из шаблона!\nПроверьте коррекность пути!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                throw new Exception();
-            }
         }
 
         /// <summary>
@@ -90,40 +34,13 @@ namespace WorkReportCreator
             }
         }
 
-        /// <summary>
-        /// Отмечает / снимает отметки со всех кнопок с практическими / лабораторными работами
-        /// </summary>
-        private void CheckAllWorks(object sender, RoutedEventArgs e)
-        {
-            string parentName = ((sender as Button).Parent as StackPanel).Name;
-            if (parentName.Contains("Practical"))
-            {
-                _practicalWorksButtons.ForEach(x => x.IsChecked = _shouldCheckAllPracticalWorks);
-                _shouldCheckAllPracticalWorks = !_shouldCheckAllPracticalWorks;
-            }
-            else if (parentName.Contains("Laboratory"))
-            {
-                _laboratoryWorksButtons.ForEach(x => x.IsChecked = _shouldCheckAllLaboratoryWork);
-                _shouldCheckAllLaboratoryWork = !_shouldCheckAllLaboratoryWork;
-            }
-        }
 
         /// <summary>
         /// Показывает форму с выбором заданий (при наличии) и файлов для отчета (при наличии)
         /// </summary>
         private void ShowFormReportsPage(object sender, RoutedEventArgs e)
         {
-            List<string> selectedLaboratoryWorks = _laboratoryWorksButtons.Where(x => x.IsChecked ?? false).Select(x => x.Content as string).ToList();
-            List<string> selectedPracticalWorks = _practicalWorksButtons.Where(x => x.IsChecked ?? false).Select(x => x.Content as string).ToList();
-            try
-            {
-                ReportsWindow reportsPage = new ReportsWindow(this, selectedLaboratoryWorks, selectedPracticalWorks, _model.Student);
-                Hide();
-                reportsPage.Show();
-            }
-            catch (Exception)
-            {
-            }
+
         }
 
         /// <summary>
