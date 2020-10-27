7f85784cf008c0148aa998645582b42b819b8772
Inozpavel <Inozpavel@mail.ru>
2020-10-27 16:46:58 +0300

    При сохранении информации о студенте файл автоматическим создается, ускорена анимация

--- ViewModels/StudentInformationWindowViewModel.cs
+++ ViewModels/StudentInformationWindowViewModel.cs
@@ -259,6 +259,7 @@ namespace WorkReportCreator.ViewModels.Commands
                 {
                     UserDataFileName = dialog.FileName
                 };
+                SaveStudentInformation(this, null);
             }
         }
 
